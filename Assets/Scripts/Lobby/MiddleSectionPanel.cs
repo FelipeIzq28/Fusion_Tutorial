318282d7843350e859c755e4fef13a53968dd22b
FelipeIzq28 <felipeizquierdo47@gmail.com>
2024-07-28 19:23:15 -0500

    Lobby part II

--- Assets/Scripts/Lobby/MiddleSectionPanel.cs
+++ Assets/Scripts/Lobby/MiddleSectionPanel.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+using Fusion;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MiddleSectionPanel : LobbyPanelBase
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("MiddleSectionPanel Vars")]
+    [SerializeField] private Button joinRandomRoomBtn;
+    [SerializeField] private Button joinRoomByArgBtn;
+    [SerializeField] private Button createRoomBtn;
+
+    [SerializeField] private TMP_InputField joinRoomByArgInputField;
+    [SerializeField] private TMP_InputField createRoomInputField;
+    private NetworkRunnerController networkRunnerController;
+    public override void InitPanel(LobbyUIManager uiManager)
     {
-        
+        base.InitPanel(uiManager);
+
+        networkRunnerController = GlobalManagers.Instance.networkRunnerController;
+        joinRandomRoomBtn.onClick.AddListener(JoinRandomRoom);
+        joinRoomByArgBtn.onClick.AddListener( ()=> CreateRoom(GameMode.Client, joinRoomByArgInputField.text));
+        createRoomBtn.onClick.AddListener(() => CreateRoom(GameMode.Host, createRoomInputField.text));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateRoom(GameMode mode, string field)
     {
-        
+        if(createRoomInputField.text.Length >= 2)
+        {
+            Debug.Log($"-----{mode}-----");
+            networkRunnerController.StartGame(mode, field);
+        }
     }
+
+    private void JoinRandomRoom()
+    {
+        Debug.Log($"-----JoinRandomRoom!-----");
+        networkRunnerController.StartGame(GameMode.AutoHostOrClient, string.Empty);
+    }
+
 }
